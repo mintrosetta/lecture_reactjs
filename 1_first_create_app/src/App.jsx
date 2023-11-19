bf6773a6c0c10d85bb7b448dee4938235c1dbe9e
Chanokchon Wongjumpa <mint.rosetta2001@gmail.com>
2023-11-19 12:43:00 +0700

    initial first react app - vite created

---
+++ 1_first_create_app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>You have read <strong>{props.count}</strong> pieces of advice.</p>
+  );
+}
